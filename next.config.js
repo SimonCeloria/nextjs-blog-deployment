59941481e62402b93809e3f3e32d79757999066f
Simon Celoria <celoriasimon@gmail.com>
2024-04-11 21:55:08 -0300

    next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,16 @@
 /**
  * @type {import('next').NextConfig}
  */
+
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
+    basePath: isProd ? '/next.js-blog-deployment' : '',
     output: 'export',
     distDir: 'dist',
     images: {
       unoptimized: true,
     },
-  }
+  };
    
   module.exports = nextConfig
